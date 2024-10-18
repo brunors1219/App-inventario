85ae48ab0817b67511408c89b5f8d2839621a6eb
Bruno Rangel <112968887+brunors1219@users.noreply.github.com>
2024-10-18 09:37:48 -0300

    Atualizar o cadProd.js
    alteração página cadastro produto

--- components/cadProd.js
+++ components/cadProd.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, StyleSheet} from "react-native";
 
 export default function Cadprod(){
     return(
-        <View>
-            <Text>Cadastro de produto</Text>
-            <TextInput placeholder="Codigo"/>
+        <View style={styles.container}>
+            <Text style={styles.text}>Cadastro de produto</Text>
+            <TextInput placeholder="Codigo"style={styles.input}/>
         </View>
     )
+}
+
+styles = StyleSheet.create{
+ container:{
+ flex:1,
+ padding:20,
+ justifyContent:'center'
+},
+
+ text:{
+  fontsize:10
+ },
+input:{
+ 
+
+}
 }
